070786430f50e566f2d76aed2021d7a5039c3bb4
fabianocruzacad <fabianocruzacad@gmail.com>
2018-09-02 10:53:10 -0300

    bootstrap4 - Adicionado Estilo

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 include 'text.php';
+include 'style.php';
 /*$texto ="Take all I have in these hands
  And multiply, God, all that I am 
  And find my heart on the altar again
@@ -39,33 +40,64 @@ $texto = str_replace("->","", $texto);
 $texto = str_replace(".'","", $texto); 
 $texto = str_replace(".","", $texto);
 $texto = str_replace("\n", " ", $texto);
-for ($i=0; $i < 100; $i++) { 
+/*for ($i=0; $i < 100; $i++) { 
 	# code...
 	$texto = str_replace("$i","", $texto);
-
-
 }
+*/
 
 
+echo "
+
+<h1> Lista de Palavras x Frequencia </h1>
+<table class='table'>
+		<thead class='thead-dark'>
+		   <tr>
+		      <th scope='col'>ID</th>
+		      <th scope='col'>Palavra</th>
+		      <th scope='col'> % </th>		      
+		    </tr>
+	    </thead>
+";
 
 $partes = explode(' ', $texto);
-
-
-echo "Palavra : Ocorrencias";
-echo "<br>";
-$palavras = array();
 foreach ($partes as $key => $value) {
+	echo "
+		<tbody>
+			<tr>
+				<td>$key</td>
+				<td>$value</td>
+				<td>?</td>
+			</tr>
+		</tbody>
+	
+	";
+}
+echo "</table>";
+
+
+/*echo "Palavra : Ocorrencias";
+echo "<br>";*/
+/*$palavras = array();
+foreach ($partes as $key => $value) {
+
 
 	if (!in_array($value, $palavras)){
 		
 		$ocorrencias = substr_count($texto, $value);
 		$palavras[$value]=$ocorrencias;
 		
-		echo "$key - $value - $ocorrencias"; 
+		echo "$key-$value-$ocorrencias"; 
 		echo "<br>";
 	}
 }
+*/
 
+
+/*echo "=================";
+echo "<pre>";
+print_r($partes);
+*/
 
 // foreach ($palavras as $key => $value) {
 // 	if ($value>2 and $value<15) {
